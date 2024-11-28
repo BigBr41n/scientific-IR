ef5a0bd1400d65d09f2f422ebd21236213039cb2
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-11-28 23:10:34 +0100

    feat: process func that open a dir read the entries and verify the extension of the file and creates a go routine to tokenize and send back an inverted index of each file in a buffered channel and mergre them in one inverted index

--- system/internals/preprocess/tokenizer.go
+++ system/internals/preprocess/tokenizer.go
@@ -1,6 +1,12 @@
 package preprocess
 
-import "os"
+import (
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+)
 
 // enriched posting node
 type PositionNode struct {
@@ -43,14 +49,66 @@ func NewTokenizer(docsPath string) TokenizerI {
 
 
 // tokenize and return the local inverted index for each document
-func tokenize(file * os.File) (InvertedIndex , error){}
+func tokenize(file fs.DirEntry, wg * sync.WaitGroup, indexChan chan <- InvertedIndex, path string ){}
 
 
 // extract the position for each word in the document
-func extractPositions(wordsSlice string , file os.File) (*[]int) {}
+//TODO : func extractPositions(wordsSlice string , file os.File) (*[]int) {}
 
 
 // open each file in the dir and pass it to Tokenize  function 
 // to get the local inverted index for each doc and then merge them in one inverted index 
-func (dp * Tokenizer) ProcessFiles() (InvertedIndex , error) {}
+func (dp * Tokenizer) ProcessFiles() (InvertedIndex , error) {
+    // wait group 
+    var wg sync.WaitGroup
+
+    // Create a new inverted index (this is the global one that will returned)
+    index := make(InvertedIndex)
+
+
+    // Read Dir 
+    entries , err := os.ReadDir(dp.docsPath)
+    if err!= nil {
+        log.Fatal(err)
+    }
+
+    // channel to pass the local indexes from go routines 
+    indexChan := make(chan InvertedIndex, len(entries))
+    
+    // pass each file to a goroutine to handle it 
+    // Iterate over each file in the directory
+    for _, entry := range entries {
+        wg.Add(1)
+        if !entry.IsDir() && filepath.Ext(entry.Name()) == ".txt" {
+            go tokenize(entry, &wg, indexChan , dp.docsPath)
+        }
+    }
+
+    // wait for all goroutines to finish
+	go func() {
+		wg.Wait()
+		close(indexChan)
+	}()
+
+
+    // merge inverted indexes into one global inverted index
+    for localIndex := range indexChan {
+		for term, postingList := range localIndex {
+			if _, exists := index[term]; !exists {
+				// If term doesn't exist, add it
+				index[term] = postingList
+			} else {
+				// Merge the posting lists
+				current := index[term]
+				for current.next != nil {
+					current = current.next
+				}
+				current.next = postingList
+			}
+		}
+	}
+
+	return index, nil
+
+}
 
