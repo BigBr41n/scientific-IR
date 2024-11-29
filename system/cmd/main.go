7e4168831b466568360e5826e0b0df7de4d03dfb
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-11-29 18:24:03 +0100

    main: create inverted index and print it

--- system/cmd/main.go
+++ system/cmd/main.go
@@ -2,9 +2,52 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/BigBr41n/scientific-IR/internals/preprocess"
 )
 
 
+
+func printInvertedIndex(index preprocess.InvertedIndex) {
+    for term, postingList := range index {
+        log.Printf("Term: %s", term)
+
+        currentPosting := postingList
+        for currentPosting != nil {
+            log.Printf("  Document: %s", currentPosting.DocID)
+
+            // Traverse the positions list
+            positions := []int16{}
+            currentPosition := currentPosting.Positions
+            for currentPosition != nil {
+                positions = append(positions, currentPosition.Position)
+                currentPosition = currentPosition.Next
+            }
+
+            log.Printf("    Positions: %v", positions)
+
+            // Move to the next posting in the list
+            currentPosting = currentPosting.Next
+        }
+    }
+}
+
+
+
 func main() {
-	log.Print("SCIENTIFIC INFORMATION RETRIEVAL")
+	log.Println("SCIENTIFIC INFORMATION RETRIEVAL")
+
+	cwd , err := os.Getwd()
+	if err!= nil {
+        log.Fatal(err)
+    }
+	log.Println("Current working directory:", cwd)
+	dirPath := filepath.Join(cwd,"/data/documents")
+	tokenizer := preprocess.NewTokenizer(dirPath)
+
+	invertedIndex , _ := tokenizer.ProcessFiles()
+
+	printInvertedIndex(invertedIndex)
 }
