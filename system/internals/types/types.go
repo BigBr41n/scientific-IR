24c92fc2f090684143acbf19f017e7f201a5dae4
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-02 18:03:03 +0100

    refactor : moving regex to single shared place

--- system/internals/types/types.go
+++ system/internals/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "regexp"
+
 // TDM with TF-IDF weights
 type TDM struct {
 	Matrix      map[string]map[string]float64 // Term -> Document -> TF-IDF Weight
@@ -7,3 +9,10 @@ type TDM struct {
 	Documents   []string                      // List of all documents
 	DocWordCount map[string]int               // Total word count for each document
 }
+
+
+// Precompiled regex for efficiency
+var (
+	PunctuationRegex = regexp.MustCompile(`[^\w\s]`) // Removes punctuation and special characters
+	NumberRegex      = regexp.MustCompile(`\d+`)    // Removes numbers
+)
