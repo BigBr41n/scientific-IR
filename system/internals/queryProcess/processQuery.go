ed8c68093d6c25b15fd8334182537b69100f4364
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-02 18:04:49 +0100

    TODO: proces the query for boolean VSM

--- system/internals/queryProcess/processQuery.go
+++ system/internals/queryProcess/processQuery.go
@@ -17,6 +17,12 @@ type Weighting struct {
 	TFIDF   float64
 }
 
+type TermTFIDF struct {
+    Term   string
+    TFIDF  float64
+}
+
+
 // exact matching
 func Classic(query string) ([]string , error){
 	var results []string
@@ -57,7 +63,9 @@ func QueryWeight(query string, TDM * types.TDM)([]float64 , error){
 
 	// remove stop words 
 	for _, word := range words {
-		word = strings.ToLower(strings.TrimSpace(strings.Trim(word, ".,!?\"'")))
+		
+		word = utils.Normalize(word)
+
 		if _, exists := stopWords[word]; exists {
 			continue
 		}
@@ -72,7 +80,6 @@ func QueryWeight(query string, TDM * types.TDM)([]float64 , error){
 
 		// stem 
 		word = preprocess.StemWords(word)
-
 
 		IDF := weighting.CalculateQueryIDF(word, TDM)
 		if IDF == 0.0 {
@@ -119,6 +126,91 @@ func QueryWeight(query string, TDM * types.TDM)([]float64 , error){
 	return queryResult, nil
 }
 
+
+
+/* func VSMBoolean(query string, TDM * types.TDM)(map[string]float64 , error){
+	qIDF := make(map[string]Weighting)
+	// Load stop words
+	stopWords, err := utils.LoadStopWords()
+    if err!= nil {
+        return nil, err
+    }
+
+	// extract words 
+	words := strings.Fields(query)
+
+	// remove stop words 
+	for _, word := range words {
+		
+		word = utils.Normalize(word)
+
+		if _, exists := stopWords[word]; exists && word != "AND" && word != "OR" && word != "NOT"{
+			continue
+		}
+		if _ , exists := qIDF[word]; exists {
+			qIDF[word] = Weighting{
+				Tf: qIDF[word].Tf + 1,
+                Idf: qIDF[word].Idf,
+                TFIDF: 0.0,
+			}
+			continue
+		}
+
+		// stem 
+		word = preprocess.StemWords(word)
+
+		IDF := weighting.CalculateQueryIDF(word, TDM)
+		if IDF == 0.0 && word != "AND" && word != "OR" && word != "NOT" {
+			// don't store the term because it don't exist in the unique terms 
+			continue
+        }
+
+		qIDF[word] = Weighting{
+			Tf: 1,
+			Idf: IDF,
+			TFIDF: 0.0,
+		}
+	}
+
+
+	countQueryLen := 0 
+	for key := range qIDF {
+		if key == "AND" || key == "OR" || key == "NOT" {
+			continue
+		}
+		countQueryLen++
+	}
+
+	if (countQueryLen == 0) {
+		return map[string]float64{}, nil
+	}
+
+	queryLenght := len(qIDF)
+
+
+	for word := range qIDF {
+		if word == "AND" || word == "OR" || word == "NOT" {
+            qIDF[word] = Weighting{
+				Tf: qIDF[word].Tf,
+				Idf: qIDF[word].Idf,
+				TFIDF: 0.0,
+			}
+        }
+		qIDF[word] = Weighting{
+			Tf: qIDF[word].Tf,
+            Idf: qIDF[word].Idf,
+			TFIDF: qIDF[word].Idf * (float64(qIDF[word].Tf) / float64(queryLenght)),
+		}
+	}
+
+	queryResult := make(map[string]float64, 0)
+
+	for word , data := range qIDF {
+		queryResult[word] =  data.TFIDF
+	}
+
+	return queryResult, nil
+} */
 
 
 
