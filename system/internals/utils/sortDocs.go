25ffe450d0dbec3418a88db6e5af1fba7163a4d8
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-02 20:51:46 +0100

    feat: sorting docs by cosine similarity

---
+++ system/internals/utils/sortDocs.go
@@ -0,0 +1,27 @@
+package utils
+
+import "sort"
+
+
+func SortResultsBySimilarity(results map[string]float64) []string {
+    type docSimilarity struct {
+        DocID    string
+        Similarity float64
+    }
+
+    similaritySlice := make([]docSimilarity, 0, len(results))
+    for docID, similarity := range results {
+        similaritySlice = append(similaritySlice, docSimilarity{DocID: docID, Similarity: similarity})
+    }
+
+    sort.Slice(similaritySlice, func(i, j int) bool {
+        return similaritySlice[i].Similarity > similaritySlice[j].Similarity
+    })
+
+    sortedDocs := make([]string, len(similaritySlice))
+    for i, entry := range similaritySlice {
+        sortedDocs[i] = entry.DocID
+    }
+
+    return sortedDocs
+}
