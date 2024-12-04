bde6d0a90f8a612473a0071f2f0cf0a4253143ba
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-04 01:54:10 +0100

    update : integrating LSI model

--- system/internals/models/models.go
+++ system/internals/models/models.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"sort"
 
 	"github.com/BigBr41n/scientific-IR/internals/preprocess"
 	queryprocess "github.com/BigBr41n/scientific-IR/internals/queryProcess"
@@ -20,7 +22,7 @@ type Data struct {
 type IrModels interface {
 	ClassicBoolean(query string) ([]string, error)
 	VSM(query string) ([]string , error)
-	//LSI(query string) ([]string , error)
+	LSI(query string) ([]string , error)
 }
 
 
@@ -115,7 +117,49 @@ func (data * Data) VSM(query string)([]string, error) {
 
 
 
-//
-//func (data * Data) LSI(query string) ([]string, error) {
-//
-//}
+func (data * Data) LSI(query string) ([]string, error) {
+    processed , err := queryprocess.QueryWeight(query, data.TDM_MATRIX, data.StopWords)
+	if err != nil {
+		return nil , err
+	}
+
+    //sort data 
+    sort.Strings(data.TDM_MATRIX.Terms)
+    sort.Strings(data.TDM_MATRIX.Documents)
+
+
+    TDM := make([][]float64, len(data.TDM_MATRIX.Terms)) 
+    for i := range TDM {
+        TDM[i] = make([]float64, len(data.TDM_MATRIX.Documents)) 
+    }
+
+    // move the TFxIDF to the matrix 
+    for docIdx , doc := range data.TDM_MATRIX.Documents {
+        for termIdx , term := range data.TDM_MATRIX.Terms {
+            if value, exists := data.TDM_MATRIX.Matrix[term][doc]; exists {
+                TDM[termIdx][docIdx] = value 
+            } else {
+                TDM[termIdx][docIdx] = 0.0 
+            }
+        }
+    }
+
+    U, SIGMA, VT := utils.SVD(TDM)
+    newQuery := queryprocess.TransformQueryAlt( processed , U , SIGMA , 3)
+
+
+    // calculate similarities 
+    result := utils.CalculateSimilarities(newQuery , VT , 3)
+    documents := make([]string ,0)
+
+
+    log.Println("the length of result : ",len(result)) // should be equal the number of docs
+
+    for idx , cos := range result {
+        if cos > 0 {
+            documents = append(documents , data.TDM_MATRIX.Documents[idx])
+        }
+    }
+
+    return documents, nil
+}
