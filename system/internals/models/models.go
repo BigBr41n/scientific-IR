0860beebfb47f05f8b6c4a41d158c8af2f524c2e
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-07 23:08:46 +0100

    feat : BM25 IR  model

--- system/internals/models/models.go
+++ system/internals/models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"math"
 	"sort"
 
 	"github.com/BigBr41n/scientific-IR/internals/preprocess"
@@ -22,6 +23,7 @@ type IrModels interface {
 	ClassicBoolean(query string) ([]string, error)
 	VSM(query string) ([]string , error)
 	LSI(query string) ([]string , error)
+    BM25(query string, k int , b float64) (map[string]float64 , error)
 }
 
 
@@ -161,4 +163,57 @@ func (data * Data) LSI(query string) ([]string, error) {
     }
 
     return documents, nil
+}
+
+
+
+// BM25 model 
+func (data * Data) BM25(query string , k int , b float64) (map[string]float64 , error) {
+
+    processed, err := queryprocess.Classic(query, data.StopWords)
+    if err != nil {
+        return nil, err
+    }
+
+    //sort data
+    sort.Strings(data.TDM_MATRIX.Terms)
+    sort.Strings(data.TDM_MATRIX.Documents)
+
+    // avg doc len
+    avgDocLen := float64(len(data.TDM_MATRIX.Documents)) / float64(len(data.TDM_MATRIX.Terms))
+
+    results := make(map[string]float64, len(data.TDM_MATRIX.Documents))
+
+    for _ , document := range data.TDM_MATRIX.Documents {
+        documentLen := float64(data.TDM_MATRIX.DocWordCount[document])
+        documentScore := 0.0
+
+        for _, term := range processed {
+            if _, exists := data.TDM_MATRIX.Matrix[term]; !exists {
+                continue
+            }
+
+            posting := (*data.InvertedIndex)[term]
+
+            tf := 0.0
+            for posting != nil && posting.DocID != document {
+                posting = posting.Next
+            }
+            if posting!= nil {
+                for pos := posting.Positions; pos!= nil; pos = pos.Next {
+                    tf++
+                }
+            }
+            
+            numDocs := len(data.TDM_MATRIX.Documents)
+            docWithTerm := len(data.TDM_MATRIX.Matrix[term]) 
+            idf := math.Log(float64(numDocs-docWithTerm) + 0.5 / float64(docWithTerm)+ 0.5)
+
+            // Compute BM25 score for the term in this document
+            documentScore += idf * ((tf * float64(k + 1)) / (tf + float64 (float64(k) * (float64((1 - b)) + b * (documentLen/avgDocLen)))))
+        }
+        results[document] = documentScore
+    }
+
+    return results , nil 
 }
