e56e0a3b795ca8662a769f01cbf9658b724328fd
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-12-08 01:17:17 +0100

    feat : using tview TUI to interact with the system

--- system/cmd/main.go
+++ system/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -9,91 +8,148 @@ import (
 
 	"github.com/BigBr41n/scientific-IR/internals/models"
 	"github.com/BigBr41n/scientific-IR/internals/preprocess"
+	queryprocess "github.com/BigBr41n/scientific-IR/internals/queryProcess"
 	"github.com/BigBr41n/scientific-IR/internals/utils"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
-//func printInvertedIndex(index preprocess.InvertedIndex) {
-//    for term, postingList := range index {
-//        log.Printf("Term: %s", term)
-//
-//        currentPosting := postingList
-//        for currentPosting != nil {
-//            log.Printf("  Document: %s", currentPosting.DocID)
-//
-//            // Traverse the positions list
-//            positions := []int16{}
-//            currentPosition := currentPosting.Positions
-//            for currentPosition != nil {
-//                positions = append(positions, currentPosition.Position)
-//                currentPosition = currentPosition.Next
-//            }
-//
-//            log.Printf("    Positions: %v", positions)
-//
-//            // Move to the next posting in the list
-//            currentPosting = currentPosting.Next
-//        }
-//    }
-//}
-
-
+var (
+	app        *tview.Application
+	mainFlex   *tview.Flex
+	queryInput *tview.InputField
+	results    *tview.TextView
+)
 
 func main() {
-	log.Println("SCIENTIFIC INFORMATION RETRIEVAL")
+	app = tview.NewApplication()
 
-	cwd , err := os.Getwd()
-	if err!= nil {
-        log.Fatal(err)
-    }
-	log.Println("Current working directory:", cwd)
-	dirPath := filepath.Join(cwd,"/data/documents")
+	// Set default black background
+	tview.Styles.PrimitiveBackgroundColor = tcell.ColorBlack
+	tview.Styles.TitleColor = tcell.ColorWhite
+	tview.Styles.BorderColor = tcell.ColorWhite
+
+	// Initialize IR components
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dirPath := filepath.Join(cwd, "/data/documents")
 	tokenizer := preprocess.NewTokenizer(dirPath)
 
-	invertedIndex , _ := tokenizer.ProcessFiles()
+	invertedIndex, _ := tokenizer.ProcessFiles()
+	TDM := preprocess.BuildTDM(invertedIndex)
+	stopWords, _ := utils.LoadStopWords()
+	models := models.NewInfoRetrievalModel(TDM, &stopWords, &invertedIndex)
 
-	//printInvertedIndex(invertedIndex)
-    // create term document matrix 
-    TDM:= preprocess.BuildTDM(invertedIndex)
-    //log.Println("Term Document Matrix:")
-    //preprocess.PrintTDM(TDM)
+	// Create header
+	header := tview.NewTextView().
+		SetTextAlign(tview.AlignCenter).
+		SetText("SCIENTIFIC INFORMATION RETRIEVAL SYSTEM").
+		SetDynamicColors(true).
+		SetBackgroundColor(tcell.NewRGBColor(0, 0, 0)) 
 
-    stopWords, _ := utils.LoadStopWords();
+	// Create query input
+	queryInput = tview.NewInputField().
+		SetLabel("[yellow]Enter query >[-]").
+		SetFieldWidth(60)
 
-    query := ""
-    fmt.Print("Enter a query: ")
-    scanner := bufio.NewScanner(os.Stdin)
+	// Create results view
+	results = tview.NewTextView().
+		SetScrollable(true).
+		SetDynamicColors(true)
 
-	// Scan the next line.
-	if scanner.Scan() {
-		query = scanner.Text()
+	results.SetBackgroundColor(tcell.NewRGBColor(0, 0, 0)) 
+
+	queryInput.SetBackgroundColor(tcell.NewRGBColor(0, 0, 0)) 
+	queryInput.SetFieldBackgroundColor(tcell.ColorWhite)   
+	queryInput.SetFieldTextColor(tcell.ColorRed)    
+
+	// Handle query submission
+	queryInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			query := queryInput.GetText()
+			results.Clear()
+	
+			preprocessedQuery, err := queryprocess.ProcessQuery(query, &stopWords)
+			if err != nil {
+				fmt.Fprintf(results, "[red]Error processing query: %v\n[-]", err)
+				app.SetFocus(queryInput) // Refocus on the input field
+				return
+			}
+	
+			// Add a title for the query results
+			fmt.Fprintf(results, "[yellow]--- Results for Query: '%s' ---[-]\n\n", query)
+	
+			// Classic Boolean
+			result, _ := models.ClassicBoolean(preprocessedQuery)
+			fmt.Fprintf(results, "[cyan]Classic Boolean Results:[-]\n")
+			if len(result) > 0 {
+				for _, doc := range result {
+					fmt.Fprintf(results, "  - %v\n", doc)
+				}
+			} else {
+				fmt.Fprintf(results, "  [gray]No results found.[-]\n")
+			}
+			fmt.Fprintf(results, "\n")
+	
+			// VSM
+			result, _ = models.VSM(preprocessedQuery)
+			fmt.Fprintf(results, "[cyan]Vector Space Model Results:[-]\n")
+			if len(result) > 0 {
+				for _, doc := range result {
+					fmt.Fprintf(results, "  - %v\n", doc)
+				}
+			} else {
+				fmt.Fprintf(results, "  [gray]No results found.[-]\n")
+			}
+			fmt.Fprintf(results, "\n")
+	
+			// LSI
+			result, _ = models.LSI(preprocessedQuery)
+			fmt.Fprintf(results, "[cyan]Latent Semantic Indexing Results:[-]\n")
+			if len(result) > 0 {
+				for _, doc := range result {
+					fmt.Fprintf(results, "  - %v\n", doc)
+				}
+			} else {
+				fmt.Fprintf(results, "  [gray]No results found.[-]\n")
+			}
+			fmt.Fprintf(results, "\n")
+	
+			// BM25
+			fmt.Fprintf(results, "[cyan]BM25 Results:[-]\n")
+			BM25, err := models.BM25(preprocessedQuery, 1, 0.75)
+			if err != nil {
+				fmt.Fprintf(results, "[red]Error in BM25: %v[-]\n", err)
+			} else if len(BM25) > 0 {
+				for idx , res := range BM25 {
+					if idx < 10 {
+						fmt.Fprintf(results, "  - %v\n", res)
+					}else {
+						break
+					}
+				}
+			} else {
+				fmt.Fprintf(results, "  [gray]No results found.[-]\n")
+			}
+	
+			// Add a footer to separate results from input
+			fmt.Fprintf(results, "\n[yellow]--- End of Results ---[-]\n\n")
+	
+			app.SetFocus(queryInput) // Refocus on the input field
+		}
+	})
+
+	// Layout
+	mainFlex = tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(header, 3, 0, false).
+		AddItem(queryInput, 3, 0, true).
+		AddItem(results, 0, 1, false)
+
+	if err := app.SetRoot(mainFlex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
 	}
-
-    models := models.NewInfoRetrievalModel(TDM , &stopWords, &invertedIndex)
-
-    result, _ := models.ClassicBoolean(query)
-
-    fmt.Printf("the result with intersection : %v\n", result)
-
-
-
-	fmt.Print("Enter a query: ")
-	// Scan the next line.
-	if scanner.Scan() {
-		query = scanner.Text()
-	}
-
-	result , _ = models.VSM(query)
-
-	fmt.Printf("the result with VSM : %v\n", result)
-
-
-
-	fmt.Print("Enter a query: ")
-	if scanner.Scan() {
-		query = scanner.Text()
-	}
-
-	result , _ = models.LSI(query)
-
-	fmt.Printf("the result with LSI : %v\n", result)
 }
